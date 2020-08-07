90617c9bf9c0c1436e0f62872124b5448d7d30be
WMXPY <wm@mengw.io>
2020-08-07 14:21:32 -0500

    fix import

--- src/encrypt.ts
+++ src/encrypt.ts
@@ -9,7 +9,7 @@ import { xorBinaryString } from "./util";
 
 export const encryptSaveFile = (save: SaveFile): string => {
 
-    const stringified: string = JSON.stringify(save);
+    const stringified: string = JSON.stringify(save, null, '\t');
     const encrypted: string = xorBinaryString(stringified);
 
     const base64: string = Buffer.from(encrypted, 'binary').toString('base64');
--- test/scenario/start.test.ts
+++ test/scenario/start.test.ts
@@ -9,18 +9,37 @@ import { readTextFile } from "@sudoo/io";
 import { expect } from 'chai';
 import * as Chance from 'chance';
 import { joinStaticPath } from '../../example/util';
-import { decryptSaveFile, SaveFile } from '../../src';
+import { decryptSaveFile, SaveFile, encryptSaveFile } from '../../src';
 
 describe('Given (Start) Scenario', (): void => {
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const chance: Chance.Chance = new Chance('scenario-start');
 
-    it('should be able to parse example', async (): Promise<void> => {
+    let simpleContent: string;
 
-        const content: string = await readTextFile(joinStaticPath('start.autosave'));
-        const parsed: SaveFile = decryptSaveFile(content);
+    let decrypted: SaveFile;
 
-        expect(typeof parsed).to.be.equal('object');
+    before(async (): Promise<void> => {
+
+        simpleContent = await readTextFile(joinStaticPath('start.autosave'));
+    });
+
+    it('should be able to parse example', (): void => {
+
+        const parsed: SaveFile = decryptSaveFile(simpleContent);
+        decrypted = parsed;
+
+        expect(typeof decrypted).to.be.equal('object');
+    });
+
+    it('should be able to stringify example', (): void => {
+
+        const stringified: string = encryptSaveFile(decrypted);
+
+        expect(typeof stringified).to.be.equal('string');
+
+        const reparsed: SaveFile = decryptSaveFile(stringified);
+        expect(reparsed).to.be.deep.equal(decrypted);
     });
 });
